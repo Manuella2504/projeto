9260002c273c75b4b5145fa60a7803e5e67a3a37
Manuella <manuella.rosario@ba.estudante.senai.br>
2025-11-20 19:12:32 -0300

    Organização inicial

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+// ROTAS DO BACKEND
 const authRoutes = require("./routes/authRoutes");
 const atividadeRoutes = require("./routes/atividadeRoutes");
 const likeRoutes = require("./routes/likeRoutes");
@@ -18,8 +20,12 @@ app.use("/likes", likeRoutes);
 app.use("/comentarios", comentarioRoutes);
 app.use("/usuarios", usuarioRoutes);
 
+// SERVIR FRONTEND
+app.use(express.static(path.join(__dirname, "../../frontend")));
+
+// ROTA PRINCIPAL → envia o index.html do frontend
 app.get("/", (req, res) => {
-    res.send("API SAEPSaúde funcionando!");
+    res.sendFile(path.join(__dirname, "../../frontend/index.html"));
 });
 
 app.listen(3000, () => {
